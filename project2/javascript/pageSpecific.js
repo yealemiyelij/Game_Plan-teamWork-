529ad4663dd5257678c3f3fb1569b07d1236ac9f
Kyla <kyladurbin15@gmail.com>
2017-12-17 14:19:31 -0500

    added logo and more js

--- project2/javascript/pageSpecific.js
+++ project2/javascript/pageSpecific.js
@@ -24,10 +24,18 @@ $("#createGroup").on("click", function (event) {
     friends = $("#addFriends").val().trim();
     var newGroup = new group(name, type, friends);
     $("#newGroupRow").append(
-        '<div class="row m-2 group">' +
+        '<div class="row m-2 group" data-toggle="collapse" aria-expanded="false" href=".friendsList">' +
+
+        '<div class="col-12">' +
+        '<div class="row">' +
         '<div class="col-5 groupName">' + '<p>' + name + '</p>' + '</div>' +
         '<div class="col-5 groupType">' + '<p>' + type + '</p>' + '</div>' +
-        '<button type="button" class="btn btn-link">' + '<a><small> Delete <small></a>' + '</button>' +
+        '<div class=col-2>' + '<button type="button" class="btn btn-link delete">' + '<a><small> Delete <small></a>' + '</button>' + '</div>' +
+        '</div>' +
+
+        '<div class="collapse col-10-offset-1 mb-3 px-2 friendsList">' + '<div class="row">' + '<div class="col-12">' + friends + '</div>' + '</div>' + '</div>' +
+        '</div>' +
+
         '</div>'
     );
     newFunction();
@@ -49,26 +57,96 @@ var eventGroup = "";
 var eventDate = "";
 
 
-function newFunction2() {
-    console.log(eventName);
-}
-
 $("#createEvent").on("click", function (event) {
+
+
     event.preventDefault();
     eventName = $("#newEventName").val().trim();
     eventGroup = $("#newEventGroup").val().trim();
     eventDate = $("#newEventDate").val().trim();
 
+    var date = eventDate;
+    var newDate = date.toString('dd-MM-yy');
+    console.log(newDate);
+
     var newEvent = new nEvent(eventName, eventGroup, eventDate);
     $("#eventRow").append(
-        '<div class="row m-2 no-gutters group">' +
-        '<div class="col-3 eventName">' + '<p>' + eventName + '<p>' + '</div>' +
-        '<div class="col-3 eventGroup">' + '<p>' + eventGroup + '</p>' + '</div>' +
-        '<div class="col-3 eventDate">' + '<small><p>' + eventDate + '</p></small>' + '</div>' +
-        '<button type="button" class="btn btn-link">' + '<a><small> Delete <small></a>' + '</button>' +
-        '</div'
+        '<div class="row m-2 group' + newEvent + '">' + '<div class="col-12" data-toggle="collapse" aria-expanded="false" href=".itinerary">' +
+
+        '<div class="row">' +
+        '<div class="col-3">' + eventName + '</div>' +
+        '<div class="col-3 eventGroup">' + eventGroup + '</div>' +
+        '<div class="col-3 eventDate">' + '<small>' + newDate + '</small>' + '</div>' +
+        '<button type="button" class="btn btn-link" id="deleteEvent">' + '<a><small> Delete </small></a>' + '</button>' +
+        '<button type="button" class="btn btn-link ' + newEvent + '" data-toggle="modal" data-target="#newEventItem">' + '<a><small> Add Event Item </small></a>' + '</button>' +
+        '</div>' +
+
+        '<div class="row collapse itinerary">' + '<div class="col-10-offset-1 mb-3 px-2">' + '<ul id="itRow" class=" ' + newEvent + '">' + '</ul>' + '</div>' + '</div>' + '</div>' +
+
+        '</div>' + '</div>'
     );
 
-    newFunction2();
-    $("#newEvent")[0].reset();
-});
+    console.log(newEvent);
+
+    $("#eventForm")[0].reset();
+})
+
+/////////Function to add Itinerary//////////
+
+class itinerary {
+    constructor(place, start, end) {
+        this.place = place;
+        this.start = start;
+        this.end = end;
+    }
+}
+
+// function createItinerary() {
+//     this.innerHTML = '<div class="row">' +
+//         '<div class="col-4">' + place + '</div>' +
+//         '<div class="col-4">' + start + '</div>' +
+//         '<div class="col-4">' + end + '</div>' +
+//         '</div>';
+//     this.className += eventName.replace(/\s/g, '');
+// }
+
+$("#createEventItem").on("click", function (event) {
+    event.preventDefault();
+    place = $("#place").val().trim();
+    start = $("#start").val().trim();
+    end = $("#end").val().trim();
+
+    var newItinerary = new itinerary(place, start, end);
+
+    // $("#itRow").append(
+    //     createItinerary()
+    // )
+
+    $("#itRow").append(
+        '<div class="row">' +
+        '<div class="col-4">' + place + '</div>' +
+        '<div class="col-4">' + start + '</div>' +
+        '<div class="col-4">' + end + '</div>' +
+        '</div>'
+    );
+
+
+    // var div = document.createElement('div')
+    // div.className = eventName.replace(/\s/g, '');
+    // div.innerHTML = createItinerary();
+    // div.onClick = div.innerHTML;
+
+
+
+    console.log(eventName)
+
+    $("#newEventItForm")[0].reset();
+})
+
+
+///////// Delete Created User Input Button//////////
+
+$("#deleteEvent").on("click", function () {
+    $(".eventName.replace(/\s/g, '')").remove();
+
+})
